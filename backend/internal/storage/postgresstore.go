4884ba7b636026d854cf5bff81ccd7dc67eb6afa
Victor Ruiz <avi.ruiz@proton.me>
2025-06-04 02:03:13 -0600

    renombrar tablas

--- backend/internal/storage/postgresstore.go
+++ backend/internal/storage/postgresstore.go
@@ -20,11 +20,12 @@ func (s *PostgresStore) GetUrl(code string) (string, error) {
 	fmt.Printf("Fetching URL for code: %s\n", code)
 	err := s.db.QueryRow(`
 		SELECT su.url
-		FROM ShortUrl su
+		FROM shorturl su
 		WHERE su.code = $1 AND su.deleted_at IS NULL
 	`, code).Scan(&url)
 
 	if err != nil {
+		fmt.Println("Error fetching URL:", err)
 		return "", err
 	}
 
@@ -36,12 +37,13 @@ func (s *PostgresStore) CreateUrl(data *models.NewShortUrl) (*models.ShortUrl, e
 	var code string
 
 	err := s.db.QueryRow(`
-		INSERT INTO ShortUrl (url, code, user_id)
+		INSERT INTO shorturl (url, code, user_id)
 		VALUES ($1, $2, $3)
 		RETURNING code
 	`, data.Url, data.Code, data.UserId).Scan(&code)
 
 	if err != nil {
+		fmt.Println("Error creating URL:", err)
 		return nil, err
 	}
 
@@ -54,12 +56,13 @@ func (s *PostgresStore) ListUrlsByUserID(userID string) []*models.ShortUrl {
 
 	rows, err := s.db.Query(`
 		SELECT id, url, code, created_at
-		FROM ShortUrl
+		FROM shorturl
 		WHERE user_id = $1 AND deleted_at IS NULL
 		ORDER BY created_at DESC
 	`, userID)
 
 	if err != nil {
+		fmt.Println("Error fetching URLs:", err)
 		return urls
 	}
 
--- backend/utils/initPostgres.sql
+++ backend/utils/initPostgres.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ShortUrl (
+CREATE TABLE shorturl (
     id SERIAL PRIMARY KEY,
     url TEXT NOT NULL,
     code VARCHAR(10) NOT NULL UNIQUE,
@@ -7,11 +7,11 @@ CREATE TABLE ShortUrl (
     deleted_at TIMESTAMP
 );
 
-CREATE TABLE Analytics (
+CREATE TABLE analytics (
     id SERIAL PRIMARY KEY,
     short_url_id INTEGER NOT NULL,
     ip_address TEXT NOT NULL,
     user_agent TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (short_url_id) REFERENCES ShortUrl (id)
+    FOREIGN KEY (short_url_id) REFERENCES shorturl (id)
 );
